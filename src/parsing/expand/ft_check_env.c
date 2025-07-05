4a09dc93f583d19ee81be8491a4c6b0dad701d95
Nada Hilal <nahilal@e2r13p2.1337.ma>
2025-07-05 15:47:40 +0100

    test

--- src/parsing/expand/ft_check_env.c
+++ src/parsing/expand/ft_check_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_env.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nahilal <nahilal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 15:46:46 by marvin            #+#    #+#             */
-/*   Updated: 2025/05/14 15:46:46 by marvin           ###   ########.fr       */
+/*   Updated: 2025/07/04 18:36:38 by nahilal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,11 @@ int	ft_check_env(const char *s1, const char *s2, size_t n)
 	{
 		if ((unsigned char)s1[i] != (unsigned char)s2[i] || i >= (n - 1))
 		{
-			if((s2[i] == ' ' || !ft_isalnum(s2[i])) && i == n -1 )
-				break;
+			if ((s2[i] == ' ' || !ft_isalnum(s2[i])) && i == n - 1)
+				break ;
 			return (1);
 		}
 		i++;
 	}
-	
 	return (0);
 }
